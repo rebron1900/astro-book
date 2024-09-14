6c2b0d56c38fcbe207f11d917ecdf2a3d1953a9c
rebron1900 <me@1900.live>
2024-09-14 18:22:00 +0800

    删掉无用的包

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,44 +2,23 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import solidJs from '@astrojs/solid-js';
 import compress from 'astro-compress';
-import linkCard from 'astro-link-card';
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://1900.live',
     markdown: {
         shikiConfig: {
-            // Choose from Shiki's built-in themes (or add your own)
-            // https://shiki.style/themes
             theme: 'dracula',
-            // Alternatively, provide multiple themes
-            // See note below for using dual light/dark themes
             themes: {
                 light: 'github-light',
                 dark: 'github-dark'
-            },
-            // Disable the default colors
-            // https://shiki.style/guide/dual-themes#without-default-color
-            // (Added in v4.12.0)
-            defaultColor: false,
-            // Add custom languages
-            // Note: Shiki has countless langs built-in, including .astro!
-            // https://shiki.style/languages
-            langs: [],
-            // Enable word wrap to prevent horizontal scrolling
-            wrap: true,
-            // Add custom transformers: https://shiki.style/guide/transformers
-            // Find common transformers: https://shiki.style/packages/transformers
-            transformers: []
+            }
         }
     },
     integrations: [
         sitemap(),
         solidJs({
             devtools: true
-        }),
-        linkCard({
-            serverCache: false
         }),
         compress({
             Image: false,
