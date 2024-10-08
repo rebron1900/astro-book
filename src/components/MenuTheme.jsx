2cb45c49309d1a2b4dcb861157be92f7863360b5
rebron1900 <me@1900.live>
2024-10-08 12:47:06 +0800

    取消menu动画以优化性能，修复memos页面动态加载bug

--- src/components/MenuTheme.jsx
+++ src/components/MenuTheme.jsx
@@ -1,5 +1,5 @@
 import { getSvg } from '../utils/help';
-import { createSignal, For, onMount, Show } from 'solid-js';
+import { createSignal, For, onMount } from 'solid-js';
 import config from '../data/config';
 
 const MenuTheme = () => {
@@ -14,6 +14,26 @@ const MenuTheme = () => {
             changeTheme(config.themes[0]);
         }
     });
+
+    const changeBtn = (func, theme, $eve) => {
+        const x = $eve.clientX;
+        const y = $eve.clientY;
+        // 计算鼠标点击位置距离视窗的最大圆半径
+        const endRadius = Math.hypot(Math.max(x, innerWidth - x), Math.max(y, innerHeight - y));
+        document.documentElement.style.setProperty('--x', x + 'px');
+        document.documentElement.style.setProperty('--y', y + 'px');
+        document.documentElement.style.setProperty('--r', endRadius + 'px');
+        // 判断浏览器是否支持document.startViewTransition
+        if (document.startViewTransition) {
+            // 如果支持就使用document.startViewTransition方法
+            document.startViewTransition(() => {
+                func(theme); // 这里的函数是切换主题的函数，调用changeBtn函数时进行传入
+            });
+        } else {
+            // 如果不支持，就使用最原始的方式，切换主题
+            func(theme);
+        }
+    };
 
     const changeTheme = (theme) => {
         localStorage.setItem('name', theme.desc);
@@ -40,6 +60,7 @@ const MenuTheme = () => {
                             </label>
                             <ul>
                                 <For each={config.themes}>{(theme, index) => <li onClick={[changeTheme, theme]}>{theme.desc}</li>}</For>
+                                {/* <For each={config.themes}>{(theme, index) => <li onClick={(e) => changeBtn(changeTheme, theme, e)}>{theme.desc}</li>}</For> */}
                             </ul>
                         </li>
                     </ul>
